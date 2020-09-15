334278dbc254fa977859ad29c7e9c8418c93ec85
lingaraj s <lingarajs25@gmail.com>
2020-09-15 14:24:13 +0530

    Updates

---
+++ precache-manifest.14d4f305c3f82a761d64bf84b2b2ee2c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d95cdd40b74f62ab7816a0111384452e",
+    "url": "/fb-clone/index.html"
+  },
+  {
+    "revision": "796bf69aa83817916704",
+    "url": "/fb-clone/static/css/main.26d1e8a7.chunk.css"
+  },
+  {
+    "revision": "7903ce8eb3c1d9915d6f",
+    "url": "/fb-clone/static/js/2.fc739f97.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/fb-clone/static/js/2.fc739f97.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "796bf69aa83817916704",
+    "url": "/fb-clone/static/js/main.8c89191f.chunk.js"
+  },
+  {
+    "revision": "70210a7fd678a62291cb",
+    "url": "/fb-clone/static/js/runtime-main.98fca3d7.js"
+  }
+]);
